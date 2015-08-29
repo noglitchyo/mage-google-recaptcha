32624fa5c125fe324050143d94254ab5c0579152
Maxime Elomari <maxime.elomari@gmail.com>
2015-08-29 04:47:30 +0200

    Fixs, checks, improvements

--- app/code/community/NoGlitchYo/GoogleRecaptcha/Model/Validator.php
+++ app/code/community/NoGlitchYo/GoogleRecaptcha/Model/Validator.php
@@ -17,12 +17,18 @@
 
 class NoGlitchYo_GoogleRecaptcha_Model_Validator
 {
+    const XML_PATH_VALIDATION_ENDPOINT_URI = 'grecaptcha/general/endpoint_uri';
+
     public function validate()
     {
         $captchaResponse = Mage::app()->getFrontController()->getAction()->getRequest()->getPost('g-recaptcha-response');
         $secret = Mage::getStoreConfig(NoGlitchYo_GoogleRecaptcha_Helper_Data::XML_PATH_SECRET_KEY);
 
-        $data = $this->_request($captchaResponse, $secret);
+        try {
+            $data = $this->_request($captchaResponse, $secret);
+        } catch (Exception $e) {
+            return false;
+        }
 
         return $data['success'];
     }
@@ -30,17 +36,20 @@ class NoGlitchYo_GoogleRecaptcha_Model_Validator
     protected function _request($captchaResponse, $secret)
     {
         try {
-            $client = new Zend_Http_Client(Mage::getStoreConfig(NoGlitchYo_GoogleRecaptcha_Helper_Data::XML_PATH_VALIDATION_ENDPOINT_URI));
+            $client = new Zend_Http_Client(Mage::getStoreConfig(self::XML_PATH_VALIDATION_ENDPOINT_URI));
             $client->setParameterGet('response', $captchaResponse);
             $client->setParameterGet('secret', $secret);
+            $client->setParameterGet('remoteip', Mage::helper('core/http')->getRemoteAddr(true));
 
             $response = $client->request();
         }
         catch (Zend_Http_Exception $e) {
-
+            Mage::logException($e);
+            Mage::getSingleton('customer/session')->addError(Mage::helper('grecaptcha')->__('Unable to validate the reCAPTCHA with Google. Please, retry later.'));
         }
         catch (Exception $e) {
-
+            Mage::logException($e);
+            Mage::getSingleton('customer/session')->addError(Mage::helper('grecaptcha')->__('Unable to validate the reCAPTCHA.'));
         }
 
         return $this->_parseResponse($response);
